65156274425938b0784a9ec1379ca75a12a3e685
Adsuarez <adsuarezq@gmail.com>
2023-05-02 17:07:12 -0500

    create objects of links for optimized navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import styles from "../styles/Navigation.module.css";
+
+const links = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Create training",
+    route: "/trainingForm",
+  },
+];
+
 export default function Navigation() {
   return (
-    <nav>
-      <h2>Capacitaciones</h2>
+    <nav className={styles.nav}>
+      <ul>
+        {links.map((link) => (
+          <li key={link.route}>
+            <Link href={link.route}>
+              <p>{link.label}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }

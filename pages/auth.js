7d636d707355ec7d5886f0b208313329c3270309
Adsuarez <adsuarezq@gmail.com>
2023-05-03 17:38:26 -0500

    create auth with forms page

---
+++ pages/auth.js
@@ -0,0 +1,36 @@
+import { handleLogin, handleSignin } from "@/helpers/handleAuth";
+import Layout from "../components/Layout";
+import styles from "../styles/FormAuth.module.css";
+
+export default function auth() {
+  return (
+    <Layout>
+      <h1>Gestión de sesión</h1>
+      <div className={styles.div}>
+        <form method="GET" onSubmit={handleLogin}>
+          <label htmlFor="loginFormEmail">email:</label>
+          <input
+            name="loginFormEmail"
+            type="email"
+            placeholder="example@domain.com"
+          />
+          <label htmlFor="loginFormPassword">password:</label>
+          <input name="loginFormPassword" type="password" />
+          <button>Iniciar sesión</button>
+        </form>
+
+        <form method="GET" onSubmit={handleSignin}>
+          <label htmlFor="signinFormEmail">email:</label>
+          <input
+            name="signinFormEmail"
+            type="email"
+            placeholder="example@domain.com"
+          />
+          <label htmlFor="signinFormPassword">password:</label>
+          <input name="signinFormPassword" type="password" />
+          <button>Registro</button>
+        </form>
+      </div>
+    </Layout>
+  );
+}

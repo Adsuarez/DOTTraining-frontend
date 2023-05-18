321839e36dbbcc7663ee4b6d63c0710762d5f271
Adsuarez <adsuarezq@gmail.com>
2023-05-18 12:44:53 -0500

    add fetching in login and signin services

--- services/users.js
+++ services/users.js
@@ -1,7 +1,22 @@
+const BASE_URL = "http://localhost:3333/api/users";
+
 export function login({ email, password }) {
-  console.log({ email, password });
+  fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    mode: "cors",
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log({ json }))
+    .catch((error) => console.error(error));
 }
 
-export function signin({ email, password }) {
-  console.log("from signin: ", { email, password });
+export async function signin({ email, password }) {
+  return fetch(BASE_URL, {
+    method: "POST",
+    mode: "cors",
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ email, password }),
+  });
 }

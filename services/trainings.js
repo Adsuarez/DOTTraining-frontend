73cbaf447a425b15ee983ba30a351a6761ee71e2
Adsuarez <adsuarezq@gmail.com>
2023-05-18 17:09:28 -0500

    add fetching to send information from validated form

--- services/trainings.js
+++ services/trainings.js
@@ -1,14 +1,23 @@
 import { trainingsList } from "../FakeTrainingsList";
+const BASE_URL = "http://localhost:3333/api/trainings";
 
 export function getTrainings() {
   return trainingsList;
 }
 
 export function createTraining({ name, quotas, date, startTime, endTime }) {
-  trainingsList.push({
-    id: 3,
+  const newTraining = {
     name,
     quotas,
     studyDays: [{ date, startTime, endTime }],
-  });
+  };
+  fetch(`${BASE_URL}`, {
+    method: "POST",
+    mode: "cors",
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify(newTraining),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log({ json }))
+    .catch((error) => console.error(error));
 }

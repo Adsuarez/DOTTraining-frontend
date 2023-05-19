03b2d3d7c1b43a743e543e89b26414584d939ec9
Adsuarez <adsuarezq@gmail.com>
2023-05-19 17:04:33 -0500

    add correct response control and returned service information

--- services/users.js
+++ services/users.js
@@ -1,15 +1,19 @@
 const BASE_URL = "http://localhost:3333/api/users";
 
-export function login({ email, password }) {
-  fetch(`${BASE_URL}/login`, {
+export async function login({ email, password }) {
+  return fetch(`${BASE_URL}/login`, {
     method: "POST",
     mode: "cors",
     headers: { "content-type": "application/json; charset=UTF-8" },
     body: JSON.stringify({ email, password }),
   })
     .then((response) => response.json())
-    .then((json) => console.log({ json }))
-    .catch((error) => console.error(error));
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => {
+      return { errorMessage: error };
+    });
 }
 
 export async function signin({ email, password }) {

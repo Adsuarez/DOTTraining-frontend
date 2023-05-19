b0015aa3f25f39d601c0b2a31f6858b717837dd9
Adsuarez <adsuarezq@gmail.com>
2023-05-19 17:02:27 -0500

    extract login form in a react component

--- pages/auth.js
+++ pages/auth.js
@@ -1,24 +1,15 @@
-import { handleLogin, handleSignin } from "@/helpers/handleAuth";
+import { handleSignin } from "@/helpers/handleAuth";
 import Layout from "../components/Layout";
 import styles from "../styles/FormAuth.module.css";
+import LoginForm from "@/components/LoginForm";
 
 export default function authPage() {
   return (
     <Layout>
       <h1>Gestión de sesión</h1>
       <div className={styles.div}>
-        <form method="GET" onSubmit={handleLogin}>
-          <label htmlFor="loginFormEmail">email:</label>
-          <input
-            name="loginFormEmail"
-            type="email"
-            placeholder="example@domain.com"
-          />
-          <label htmlFor="loginFormPassword">password:</label>
-          <input name="loginFormPassword" type="password" />
-          <button>Iniciar sesión</button>
-        </form>
-
+        <LoginForm />
+        <h2>Registro</h2>
         <form method="GET" onSubmit={handleSignin}>
           <label htmlFor="signinFormEmail">email:</label>
           <input

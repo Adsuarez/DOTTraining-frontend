0a764d8e85cefbad76778e2725f8147dfa789c35
Adsuarez <adsuarezq@gmail.com>
2023-05-23 15:08:22 -0500

    correct the mistake with upper case with context import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import UserContextProvider from "@/context/userContext";
+import UserContextProvider from "@/context/UserContext";
 
 export default function App({ Component, pageProps }) {
   return (

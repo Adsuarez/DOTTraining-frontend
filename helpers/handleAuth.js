3ff59d7d3235f67d4ae1426af4c3985924642453
Adsuarez <adsuarezq@gmail.com>
2023-05-24 17:01:38 -0500

    remove unnecesary file

--- helpers/handleAuth.js
+++
@@ -1,8 +0,0 @@
-import { signin } from "../services/users.js";
-
-export const handleSignin = (event) => {
-  event.preventDefault();
-  const { signinFormEmail: email, signinFormPassword: password } =
-    Object.fromEntries(new FormData(event.target));
-  signin({ email, password });
-};

c03f56c8db6142f026b48f996028190c8a85d38c
Adsuarez <adsuarezq@gmail.com>
2023-05-24 12:12:38 -0500

    upgrade service to make real fetching to server

--- services/trainings.js
+++ services/trainings.js
@@ -1,9 +1,19 @@
-import { trainingsList } from "../FakeTrainingsList";
 const BASE_URL = "http://localhost:3333/api/trainings";
 
-export function getTrainings() {
-  return trainingsList;
-}
+export const getTrainings = async () => {
+  return fetch(BASE_URL, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => console.error({ error }));
+};
 
 export async function createTraining(newTraining, token) {
   const { name, quotas, date, startTime, endTime } = newTraining;

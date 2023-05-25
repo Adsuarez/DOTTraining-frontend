820c7cff575361b83c3aaf20ea71161174818cfc
Adsuarez <adsuarezq@gmail.com>
2023-05-25 10:51:29 -0500

    add reactive according to signing service response

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { signin } from "../services/users.js";
 
 export function SignInForm() {
+  const [error, setError] = useState({ status: false, message: "📝" });
   const handleSignin = (event) => {
     event.preventDefault();
     const { signinFormEmail: email, signinFormPassword: password } =
       Object.fromEntries(new FormData(event.target));
-    signin({ email, password });
+    signin({ email, password }).then((jsonResponse) => {
+      if (jsonResponse.errorMessage)
+        return setError({
+          status: true,
+          message: `❌ ${jsonResponse.errorMessage}`,
+        });
+      return setError({
+        status: false,
+        message: `✔️ welcome, please login to continue`,
+      });
+    });
   };
 
   return (
     <>
       <h2>Registro</h2>
+      {error.message}
       <form method="GET" onSubmit={handleSignin}>
         <label htmlFor="signinFormEmail">email:</label>
         <input

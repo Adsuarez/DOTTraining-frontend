3037c0f68ee1012c6a445dd801b5838acfc0e0a5
Adsuarez <adsuarezq@gmail.com>
2023-05-29 13:17:50 -0500

    add conditional rendering according to permissions of user

--- components/Navigation.js
+++ components/Navigation.js
@@ -7,30 +7,37 @@ const links = [
   {
     label: "Home",
     route: "/",
+    authRequired: 0,
   },
   {
     label: "Create training",
     route: "/new-training",
+    authRequired: 1,
   },
   {
     label: "Authentication",
     route: "/auth",
+    authRequired: 0,
   },
 ];
 
 export default function Navigation() {
   const { user } = useContext(UserContext);
+  const permissions = user ? user.permissions : 0;
 
   return (
     <nav className={styles.nav}>
       <ul>
-        {links.map((link) => (
-          <li key={link.route}>
-            <Link href={link.route}>
-              <p>{link.label}</p>
-            </Link>
-          </li>
-        ))}
+        {links.map((link) => {
+          if (link.authRequired == permissions)
+            return (
+              <li key={link.route}>
+                <Link href={link.route}>
+                  <p>{link.label}</p>
+                </Link>
+              </li>
+            );
+        })}
         <li>{user?.username}</li>
       </ul>
     </nav>

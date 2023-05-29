3f280b140213f51661955ea99e55f8cf3b2e193f
Adsuarez <adsuarezq@gmail.com>
2023-05-29 13:18:30 -0500

    add protected rout to trainingForm ...pending solve error

--- components/TrainingForm.js
+++ components/TrainingForm.js
@@ -1,11 +1,18 @@
+"use client";
+
 import { useState, useContext } from "react";
 import { createTraining } from "@/services/trainings";
 import { validateForm } from "@/helpers/validateForm";
 import { UserContext } from "@/context/UserContext";
+import { useRouter } from "next/router";
 
 export function TrainingForm() {
+  const router = useRouter();
   const { user } = useContext(UserContext);
   const [error, setError] = useState({ status: false, message: "✔️" });
+
+  if (!user || user.permissions !== 1) return router.push("/");
+
   const token = user?.token;
   const permissions = user?.permissions;
   const handleSubmit = (event) => {
